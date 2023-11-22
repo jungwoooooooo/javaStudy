bfeaf97ca29573c5a2d132c6fa7053faf4360c1f
jungwoooooooo <144558596+jungwoooooooo@users.noreply.github.com>
2023-11-22 23:15:46 +0900

    첫번째

---
+++ src/ex01/OpenChallenge1.java
@@ -0,0 +1,13 @@
+package ex01;
+
+public class OpenChallenge1 {
+    public static void main(String[] args){
+        String name = "JungWoo Jo";
+        int years = 20;
+        String jop = "Depart of Computer Engineering";
+
+        System.out.println(name);
+        System.out.println(years + " years old");
+        System.out.println(jop);
+    }
+}
